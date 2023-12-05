027e33de0a9486187d48e7bbdda251232116f24b
Enxo Rodrigues <enzo.orodrigues03@gmail.com>
2023-12-05 20:31:28 -0300

    Esquece

---
+++ Stage04/primeirosPassos/comentarios.js
@@ -0,0 +1,13 @@
+//Consigo fazer comentários e esses serão ignorados
+
+console.log("Meu code será executado") // Minha linha de code printa uma mensagem no console
+
+
+console.log("Ainda será executado")
+
+/*Posso comentar 
+  em mais de uma
+  linha
+  */
+
+//console.log("Não será executado")
