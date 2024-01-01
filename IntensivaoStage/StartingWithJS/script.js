db8222da10d3f99b7330d1da73ed889068c7e762
Enzo de Oliveira Rodrigues <enzo.orodrigues03@gmail.com>
2024-01-01 18:08:04 -0300

    Exercises Js

---
+++ IntensivaoStage/StartingWithJS/script.js
@@ -0,0 +1,34 @@
+
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+ 
+let message = "Hello world!"
+
+window.alert(message)
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+const n1 = 10
+const n2 = "10"
+
+console.log( typeof n2)
+let sum = n1 + n2
+console.log(sum)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+let isNumber = (typeof n1 === "number") ? "É um número" : "Não é um número"
+console.log(isNumber)
+
+    
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let string = "Eu sou uma string"
+let isString = (typeof string === "string") ? "É uma String " : "Não é uma String"
+console.log(isString)
+  78
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
