bb857ec7a801b06280e6e9e6cfa986fca6a47056
Enzo Rodrigues <enzorodrigues@Enzos-MacBook-Air.local>
2024-01-30 07:52:02 -0300

    ES6 Modules

---
+++ Stage05/ES6Modules/main.js
@@ -0,0 +1,9 @@
+// import defaultFunction , {greeting, dateNow}from './utils.js' //Nessa linha ele importa e já executa o arquivo
+import {sum as soma} from './utils.js' //Importa sum de utils e troca o nome para Soma!
+import * as calculator from './utils.js'
+
+
+console.log(calculator)
+// defaultFunction("Enzo Rodrigues")
+
+console.log(soma(1, 2))
--- Stage05/ES6Modules/utils.js
+++ Stage05/ES6Modules/utils.js
@@ -1,6 +1,44 @@
-export {h1}
+// const dateNow =  new Date()
 
-const h1 = document.querySelector("h1")
+// const greeting = name => `Hello ${name}`
+
+// const sum = (a , b) => {
+//    return a + b
+// }
+
+// export default (argument)=> {
+//     console.log(`${dateNow}, ${greeting(argument)}`)
+// }
+
+
+
+// export {dateNow, greeting, sum} /*Named Export é necessário utilizar o nome do arquivo/dado
+// que você queira exportar, igualzinho ao que foi definido em seu arquivo e na
+// importação a mesma coisa.
+
+
+// // export default message /* Quando utilizamos o Default, podemos utilizar qualquer
+// // nome para chamar o dado em questão na importação*/
+
+export const sum = (a, b) => {
+    return a + b
+}
+
+export const sub = (a, b)=> {
+    return a - b
+}
+
+export const mult = (a , b) =>{
+    return a * b
+}
+
+
+
+
+
+
+
+
 
 
 
