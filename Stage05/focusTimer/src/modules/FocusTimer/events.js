fe98bd3a628011551082a472fb6b36da801a0413
Enzo de Oliveira Rodrigues <enzo.orodrigues03@gmail.com>
2024-02-05 08:00:11 -0300

    Iniciando a construção do timer

---
+++ Stage05/focusTimer/src/modules/FocusTimer/events.js
@@ -0,0 +1,18 @@
+import { controls } from "./elements.js";
+import * as actions from "./actions.js"
+
+export function registerControls (){
+  //Fique observando em qual elemento ocorreu o meu clique
+  controls.addEventListener("click", (event)=>{
+    
+    const action =   event.target.dataset.action
+
+    if(typeof actions[action] != "function"){
+      return
+    }
+
+    //Acessando uma propriedade de um objeto pela chave valor e executando
+    actions[action]()
+  })
+
+}
---
+++ Stage05/focusTimer/src/modules/main.js
@@ -0,0 +1,10 @@
+import "./themes.js";
+import * as FocusTimer from "./FocusTimer/index.js"
+
+
+FocusTimer.start(10, 10)
+
+
+
+
+
