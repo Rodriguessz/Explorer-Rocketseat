6b72ff06131679162961719fd0476ad1d43baaff
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-04-15 19:16:13 -0300

    Manipulando e exibindo dados no HTML com Js

--- Stage06/advancing_promises/src/js/modules/Favorites.js
+++ Stage06/advancing_promises/src/js/modules/Favorites.js
@@ -1,27 +1,113 @@
-export class Favorites{
+export class FavoritesTable{
     constructor(root){
         //Procura o elemento raiz do nosso projeto
         this.root = document.querySelector(root)
-    }    
+        this.tbody = this.root.querySelector("table tbody")
+
+        //Carrega os dados 
+        this.loadData()
+    }
+
+    loadData(){
+        this.entries = [
+            {
+                login: "rodriguessz",
+                name: "Enzo Rodrigues",
+                public_repos: "08",
+                followers: "06"
+            },
+
+             {
+                login: "rafafaaa",
+                name: "Rafael Cristofali",
+                public_repos: "08",
+                followers: "06"
+            },
+
+            {
+                "login": "diego3g",
+                "name": "Diego fernandes",
+                "public_repos": 15,
+                "followers": 120
+            },
+            {
+                "login": "maykbrito",
+                "name": "Mayk Brito",
+                "public_repos": 15,
+                "followers": 120
+            }
+
+        ]
+
+    
+    }
+
 }
 
 
-export class FavoritesView extends Favorites{
+export class FavoritesView extends FavoritesTable{
     constructor(root){
         //Construtor super chama o construtor do classe pai 
         super(root)
 
         this.updateDisplay()
+        this.fillUserRow()
     }
 
     updateDisplay(){
         this.removeAllTr()
+
+    }
+
+    createTr(loginUser,userName, repos, followers){
+        const tr = document.createElement("tr")
+
+        tr.innerHTML = `
+             <tr>
+                <td class="user">
+                    <img src="https://github.com/${loginUser}.png" width="25px" alt="Imagem de perfil do gihub">
+                     <a href="https://github.com/${loginUser}" target="_blank">
+                        
+                        <p>${userName}</p>
+                        <span>${loginUser}</span>
+    
+                    </a>
+                </td>
+    
+                <td class="repositories">
+                   ${repos}
+                </td>
+    
+                <td class="followers">
+                   ${followers}
+                </td>
+    
+                <td class="remove">
+                    <button>
+                        &times;
+                    </button>
+                   
+                 </td>
+            </tr>
+        
+        
+        
+        `
+
+        return tr
+    }
+    
+    fillUserRow(){
+         const data = this.entries
+
+        data.forEach(user =>{
+            const rowUser = this.createTr(user.login,user.name, user.public_repos, user.followers)
+            this.tbody.appendChild(rowUser)
+        })
     }
 
     removeAllTr(){
-        const tbody = this.root.querySelector("tbody")
-
-        tbody.querySelectorAll("tr")
+        this.tbody.querySelectorAll("tr")
         .forEach(tr =>{
             tr.remove()
         })
