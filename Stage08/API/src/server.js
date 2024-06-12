6e2d94aa30db604acb8424d65b85aa2ced279c70
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-06-12 19:31:48 -0300

    Método GET com Express

--- Stage08/API/src/server.js
+++ Stage08/API/src/server.js
@@ -4,9 +4,10 @@ const express = require('express');
 //Const App - Inicializa o meu express;
 const app = express();
 
+//Definindo uma rota e atrelando a ela um método GET
+app.get("/", (request, response ) => response.send("BEM-VINDO AO SISTEMA"))
+
 //Porta em que meu servidor irá rodar
 const PORT = 3333;
-
 //Listen(Porta , callback ) - Inicializa minha API na porta 3333 e quando estiver pronto, executa o callback
-
-app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`))
