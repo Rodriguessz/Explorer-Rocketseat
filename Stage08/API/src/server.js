8c3f378d0578673989c5bf67602aa43e3365d87a
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-06-17 19:44:24 -0300

    Query Params

--- Stage08/API/src/server.js
+++ Stage08/API/src/server.js
@@ -15,6 +15,18 @@ app.get("/user/:id/:username", (request, response ) => {
     
     })
 
+app.get("/products", (request, response) => {
+
+    //Recuperando parâmetros de busca
+    let {category, limit} = request.query
+
+    category = category ? category : "SEM FILTRO"
+    limit = limit ? limit : "SEM LIMITE"
+    
+    //Enviando uma resposta para o cliente
+    response.send(`Filtro de categoria: ${category} e Limite de registros por página: ${limit}`)
+})
+
 //Porta em que meu servidor irá rodar
 const PORT = 3333;
 //Listen(Porta , callback ) - Inicializa minha API na porta 3333 e quando estiver pronto, executa o callback
