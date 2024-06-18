4a3c1026184534c6da0b2b747bb5d53c92dc3a4a
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-06-18 20:06:10 -0300

    Body params e Méotodo POST

--- Stage08/API/src/server.js
+++ Stage08/API/src/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 //Const App - Inicializa o meu express;
 const app = express();
 
+//Indicando o tipo de informação que será retornada via body para nossa app
+app.use(express.json())
+
 //Definindo uma rota e atrelando a ela um método GET
 app.get("/user/:id/:username", (request, response ) => {
     
@@ -15,16 +18,27 @@ app.get("/user/:id/:username", (request, response ) => {
     
     })
 
-app.get("/products", (request, response) => {
+app.get("/users", (request, response) => {
 
-    //Recuperando parâmetros de busca
-    let {category, limit} = request.query
+    //Recuperando parâmetros de busca ( Query Params)
+    let {name, limit} = request.query
 
-    category = category ? category : "SEM FILTRO"
+    name = name ? name : "SEM FILTRO"
     limit = limit ? limit : "SEM LIMITE"
     
     //Enviando uma resposta para o cliente
-    response.send(`Filtro de categoria: ${category} e Limite de registros por página: ${limit}`)
+    response.send(`Filtro por nome: ${name} e Limite de registros por página: ${limit}`)
+})
+
+//Defininindo uma rota e atrelando a ela um método POST
+
+app.post("/create/user" , (request, response) => {
+
+    //Desestruturando o objeto retornado no body da minha request
+    const {name , email , password} = request.body
+
+    //Enviando um json com as informações recuperadas
+    response.json({name, email, password})
 })
 
 //Porta em que meu servidor irá rodar
