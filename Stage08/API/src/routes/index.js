b8ecdba2988adbc2ae3e84e09d2faa6a23f6785e
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-06-19 21:00:56 -0300

    Estrutura do projeto: Modularizando rotas

---
+++ Stage08/API/src/routes/index.js
@@ -0,0 +1,21 @@
+//Roteador principal - Centraliza as rotas da aplicação
+
+//Funcionalidade Router permite definir rotas de forma modular
+const { Router } = require("express");
+
+//Inicializa uma instância de Router
+const router = Router();
+
+//Importando os roteador espeficios
+
+const userRouter = require("./user.routes")
+
+//Declarando rotas mães para recursos especificos - Callback será o carregamento do router especifico
+router.use("/user", userRouter);
+
+
+
+
+
+//Exportando nosso router
+module.exports = router;
--- Stage08/API/src/server.js
+++ Stage08/API/src/server.js
@@ -4,42 +4,15 @@ const express = require('express');
 //Const App - Inicializa o meu express;
 const app = express();
 
+//Importando o roteador principal
+const router = require("./routes")
+
 //Indicando o tipo de informação que será retornada via body para nossa app
 app.use(express.json())
 
-//Definindo uma rota e atrelando a ela um método GET
-app.get("/user/:id/:username", (request, response ) => {
-    
-    //Recuperando parâmetros de rota
-    const {id, username} = request.params
+//Indicando para nossa aplicação quais rotas ela enxerga
+app.use(router)
 
-    //Enviando uma resposta para o cliente
-    response.send(`Bem vindo ao sistema: ${username}-${id}`)
-    
-    })
-
-app.get("/users", (request, response) => {
-
-    //Recuperando parâmetros de busca ( Query Params)
-    let {name, limit} = request.query
-
-    name = name ? name : "SEM FILTRO"
-    limit = limit ? limit : "SEM LIMITE"
-    
-    //Enviando uma resposta para o cliente
-    response.send(`Filtro por nome: ${name} e Limite de registros por página: ${limit}`)
-})
-
-//Defininindo uma rota e atrelando a ela um método POST
-
-app.post("/create/user" , (request, response) => {
-
-    //Desestruturando o objeto retornado no body da minha request
-    const {name , email , password} = request.body
-
-    //Enviando um json com as informações recuperadas
-    response.json({name, email, password})
-})
 
 //Porta em que meu servidor irá rodar
 const PORT = 3333;
