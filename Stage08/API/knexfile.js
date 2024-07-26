806d7cb826e9999787237516ed56539a5454bd31
Enzo Rodrigues <devenzo@MacBook-Pro-de-Enzo.local>
2024-07-26 00:25:44 -0300

    Implementando estrategia de migrations utilizando Knex.js

--- Stage08/API/knexfile.js
+++ Stage08/API/knexfile.js
@@ -6,6 +6,15 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "kenx",
+        "migrations",
+      ),
+    },
     useNullAsDefault: true,
   },
 };
