9084a701c60dd11fcaebbf9ea951d441dc45af1c
Enzo Rodrigues <devenzo@MacBook-Pro-de-Enzo.local>
2024-07-29 23:24:54 -0300

    Migrate para tags e links"

---
+++ Stage08/API/src/database/knex/migrations/20240730001039_createLinks.js
@@ -0,0 +1,17 @@
+exports.up = (knex) => {
+  return knex.schema.createTable("links", (table) => {
+    table.increments("id");
+    table.text("url");
+    table
+      .integer("note_id")
+      .references("id")
+      .inTable("users")
+      //OnDelete(Cascade) - Se por acaso a nota em que esse link estiver vinculado for deleteda, automaticamente delete o link também
+      .onDelete("cascade");
+    table.timestamp("created_at").default(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("links");
+};
