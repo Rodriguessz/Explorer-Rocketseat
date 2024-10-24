3583f6c502694ecb8d074ec5b00935d1a3604717
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2024-10-23 22:52:01 -0300

    Redirecionamento de rotas

--- stage09/rockeatnotes/src/routes/index.jsx
+++ stage09/rockeatnotes/src/routes/index.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
+//Importando hook useAuth para obter informações compartilhadas pelo authProvider
+import { useAuth } from '../hooks/auth'
 
-export const Routes = () => {
+
+export const Routes = () => {  
+    const { user } = useAuth();
     return(
         <>
             <BrowserRouter>
-                <AuthRoutes />
+                {!user ? (<AuthRoutes />) : <AppRoutes />}
             </BrowserRouter>
         </>
     )
