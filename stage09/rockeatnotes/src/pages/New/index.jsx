9d73c41d5dfa9d5acc8cc960baa0e1f1aa9fbf91
Enzo <enzo.orodrigues03@gmail.com>
2024-10-29 12:05:46 -0300

    Adicionando links no cadastro de ntoas

--- stage09/rockeatnotes/src/pages/New/index.jsx
+++ stage09/rockeatnotes/src/pages/New/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Container, Form } from "./styles";
 
 import { Header } from "../../components/Header";
@@ -9,17 +11,34 @@ import { Button } from "../../components/Button";
 
 import { Link } from "react-router-dom";
 
-
-
-
-
-
 export const New = () => {
-    return(
+
+    //Links relacionados a nota que o usuário está criando
+    const [links, setLinks] = useState([]);
+
+    //Novo link que o usuário irá adicionar a nota;
+    const [newLink, setNewLink] = useState("");
+
+    //Adiciona um link relacionado a nota no estado de links;
+    function handleAddLink() {
+        //Adiciona o novo link adicionado pelo usuário no estado dos links já existentes;
+        setLinks(prevState => [...prevState, newLink]);
+
+        //Limpa o estado de newLink;
+        setNewLink("")
+    }
+
+    //Remove o link especifico clicado pelo usuário
+    function handleRemoveLink(deletedIndex){
+        //Utiliza o filter para retornar todos os links que forem diferentes ao que o usuário quer remover
+        setLinks(prevState => prevState.filter((link, index) => index != deletedIndex))
+    }
+
+    return (
         <Container>
-            
+
             <Header />
-            
+
             <main>
                 <Form>
 
@@ -29,22 +48,41 @@ export const New = () => {
                     </header>
 
                     <Input placeholder="Título" />
-                    
+
                     <Textarea placeholder="Observações" />
 
                     <Section title="Links úteis" >
-                        <NoteItem value="https://rockeatseat.com" />
-                        <NoteItem  placeholder="Novo link" isNew />
 
+                        {/* Mapeia os links adicionados pelo usuário, renderizando o componente noteItem para cada um deles */}
+                        {
+                            links.map((link, index) => {
+                                return (
+                                    <NoteItem
+                                        key={String(index)}
+                                        value={link}
+                                        onclick={() => {handleRemoveLink(index)}}
+                                    />
+                                )
+                            })
+                        }
+
+
+                        <NoteItem
+                            placeholder="Novo link"
+                            value={newLink} /* Define o valor padrão do input sendo o valor do estado newLink */
+                            isNew
+                            onChange={event => setNewLink(event.target.value)} /* Adiciona o valor digitado pelo usuário ao estado newLink */
+                            onclick={handleAddLink}
+                        />
                     </Section>
 
-                    <Section title="Marcadores" >             
+                    <Section title="Marcadores" >
                         <div className="tags">
                             <NoteItem value="React" />
-                            <NoteItem  placeholder="Nova tag" isNew />
+                            <NoteItem placeholder="Nova tag" isNew />
                         </div>
                     </Section>
-                    
+
                     <Button title="Salvar" />
                 </Form>
             </main>
